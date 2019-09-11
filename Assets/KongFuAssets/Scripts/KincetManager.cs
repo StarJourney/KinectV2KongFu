6c8a8673d957725d63886527d1d57640b7db9244
StarJouerneyInMac <15611826201@163.com>
2019-09-11 21:57:40 +0800

    初始

---
+++ Assets/KongFuAssets/Scripts/KincetManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eState { eWait, eStart, eQuit }
+public class CusKincetManager
+{ 
+    public eState mCurState = eState.eWait;
+    public int mHalfWidth = 1920 / 2;
+    public int mHalfHeigth = 1080 / 2;
+    public Vector2Int mLeftPos = Vector2Int.zero;
+    public Vector2Int mRightPos = new Vector2Int(1920/2,0);
+
+    public Rect mVideoScreenRect;
+
+    public void KinectManager()
+    {
+        mVideoScreenRect = new Rect(mRightPos,new Vector2(mHalfWidth,mHalfHeigth));
+
+    }
+    static CusKincetManager mKinectManager;
+    public static CusKincetManager Instance
+    {
+        get
+        {
+            if (mKinectManager != null)
+                return mKinectManager;
+            else return mKinectManager = new CusKincetManager();
+                     
+        }
+    }
+
+    
+}
