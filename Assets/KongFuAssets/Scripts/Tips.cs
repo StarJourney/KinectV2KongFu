493ae7d201e21d8e55b00b328b19cb1bfa067172
StarJouerneyInMac <15611826201@163.com>
2019-09-13 02:55:47 +0800

    场景建设

---
+++ Assets/KongFuAssets/Scripts/Tips.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tips  
+{
+    public static readonly string WAIT_PLAYER = "等待玩家进入，请站在1.5米的位置处";
+    public static readonly string WAIT_START = "等待玩家开始游戏";
+    public static readonly string WAIT_PRATICING = "正在练习中";
+}
